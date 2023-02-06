4311678aa9762d38152be1cda5377c687df2d6da
DevDokus <49053928+DevDokus@users.noreply.github.com>
2023-02-06 03:57:48 +0100

    patch

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,8 +5,8 @@ description 'DokusCore Banking System - http://DokusCore.com'
 author 'DevDokus'
 fx_version "adamant"
 games {"rdr3"}
--- version '2.7.2'
-version '-----' -- For Development
+version '2.7.3'
+-- version '-----' -- For Development
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 client_scripts {
